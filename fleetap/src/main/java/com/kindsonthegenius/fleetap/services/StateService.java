5da83583d7ba461ddab886c4fc1fa51322bb98f0
Akif-jpg <akifesadi193@gmail.com>
2021-07-15 20:09:30 +0300

    I edited html page of states

--- fleetap/src/main/java/com/kindsonthegenius/fleetap/services/StateService.java
+++ fleetap/src/main/java/com/kindsonthegenius/fleetap/services/StateService.java
@@ -1,5 +1,32 @@
 package com.kindsonthegenius.fleetap.services;
 
-public class StateService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kindsonthegenius.fleetap.models.State;
+import com.kindsonthegenius.fleetap.repositories.StateRepository;
+
+@Service
+public class StateService {
+	@Autowired
+	StateRepository stateRepository;
+	
+	public List<State> getStates(){
+		return stateRepository.findAll();
+	}
+	
+	public void save(State state) {
+		stateRepository.save(state);
+	}
+	
+	public void delete(Integer id) {
+		stateRepository.deleteById(id);
+	}
+	
+	public Optional<State> findById(Integer id){
+		return stateRepository.findById(id);
+	}	
 }
